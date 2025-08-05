e357efa2525f813314c4c1b9fd216de37eaadef1
Sabari Poornesh S <geetaclass425@gmail.com>
2025-08-05 08:20:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,4 +28,5 @@ def logout():
     return render_template('logout.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
